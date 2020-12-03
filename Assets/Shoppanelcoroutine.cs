fc3ca41967f80ab40ef6de37e715f3a393acc8fc
Dhaval9773 <68271211+Dhaval9773@users.noreply.github.com>
2020-12-03 11:40:06 +0530

    1

--- Assets/Shoppanelcoroutine.cs
+++ Assets/Shoppanelcoroutine.cs
@@ -9,4 +9,28 @@ public class Shoppanelcoroutine : MonoBehaviour
     public RectTransform shopupperpanel;
     public RectTransform currencyGameplayDealsPanel;
     public RectTransform firstrechargepanel;
+    public RectTransform gemsCoinsPanel;
+    
+    private void OnEnable()
+    {
+        StartCoroutine(RewardChestcoroutine());
+    }
+
+    IEnumerator RewardChestcoroutine()
+    {
+        rewardchest.transform.localScale=new Vector2(.1f,.1f);
+        while (rewardchest.transform.localScale.x<1.2 && rewardchest.transform.localScale.y<1.2)
+        {
+            yield return new WaitForSeconds(.01f);
+            rewardchest.transform.localScale=new Vector2(rewardchest.transform.localScale.x+.1f,rewardchest.transform.localScale.y+.1f);
+            //print("scale");
+        }
+         
+        while (rewardchest.transform.localScale.x>1 && rewardchest.transform.localScale.y>1)
+        {
+            yield return new WaitForSeconds(.01f);
+            rewardchest.transform.localScale=new Vector2(rewardchest.transform.localScale.x-.01f,rewardchest.transform.localScale.y-.01f);
+            //print("scale");
+        }
+    }
 }
