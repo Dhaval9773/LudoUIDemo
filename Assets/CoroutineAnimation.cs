a3cf006e2b1e9a9410dccc683d84ca189a6b20ab
Dhaval9773 <68271211+Dhaval9773@users.noreply.github.com>
2020-12-03 17:04:19 +0530

    1

--- Assets/CoroutineAnimation.cs
+++ Assets/CoroutineAnimation.cs
@@ -12,7 +12,7 @@ public class CoroutineAnimation : MonoBehaviour
     public RectTransform chatb;
     public RectTransform ranksettingpanel;
     public List<GameObject> btn;
-   
+    public float speed;
     
 
     private void OnEnable()
@@ -23,91 +23,53 @@ public class CoroutineAnimation : MonoBehaviour
         StartCoroutine(chatbutton());
         StartCoroutine(shopsocialcoroutine());
         StartCoroutine(ranksetting());*/
-        StartCoroutine(mainpanel());
+        StartCoroutine(eventshopsocial());
+        StartCoroutine(upperpanelcoroutine());
+        StartCoroutine(chatranksetting());
         StartCoroutine(Delay(btn));
         
     }
-    IEnumerator mainpanel()
-    {
-        upperpanel.anchoredPosition=new Vector2(0,237);
-        
-        while (upperpanel.anchoredPosition.y>1 )
-        {
-            //yield return new WaitForSeconds(0.001f);
-            upperpanel.anchoredPosition=new Vector2(0,upperpanel.anchoredPosition.y-1);
-            
-            //print(upperpanel.anchoredPosition.y);
-            yield return null;
-        }
-    }
 
-    IEnumerator upperpanelcoroutine()
-    {
-        upperpanel.anchoredPosition=new Vector2(0,237);
-        
-        while (upperpanel.anchoredPosition.y>1 )
-        {
-            //yield return new WaitForSeconds(0.001f);
-            upperpanel.anchoredPosition=new Vector2(0,upperpanel.anchoredPosition.y-1);
-            
-            //print(upperpanel.anchoredPosition.y);
-            yield return null;
-        }
-    }
-
-    IEnumerator shopsocialcoroutine()
+    IEnumerator eventshopsocial()
     {
         shopsocial.anchoredPosition=new Vector2(-261,1);
-        while (shopsocial.anchoredPosition.x<1)
-        {
-            //yield return new WaitForSeconds(0.001f);
-            //print(shopsocial.anchoredPosition.x);
-            shopsocial.anchoredPosition=new Vector2(shopsocial.anchoredPosition.x+1,1);
-            //print(upperpanel.anchoredPosition.y);
-            yield return null;
-        }
-        
-    }
-
-    
-
-    IEnumerator eventbutton()
-    {
         eventb.anchoredPosition=new Vector2(-222,-1);
         while (eventb.anchoredPosition.x<0.7f)
         {
-           
-            eventb.anchoredPosition=new Vector2(eventb.anchoredPosition.x+1,-1);
-            
+            eventb.anchoredPosition=new Vector2(eventb.anchoredPosition.x+4,-1);
             yield return null;
         }
-        
+        while (shopsocial.anchoredPosition.x<1)
+        {
+            shopsocial.anchoredPosition=new Vector2(shopsocial.anchoredPosition.x+4,1);
+            yield return null;
+        }
     }
-    IEnumerator chatbutton()
+    IEnumerator upperpanelcoroutine()
     {
-        chatb.anchoredPosition = new Vector2(282, 0.2f);
+        upperpanel.anchoredPosition=new Vector2(0,237);
+        while (upperpanel.anchoredPosition.y>1 )
+        {
+            upperpanel.anchoredPosition=new Vector2(0,upperpanel.anchoredPosition.y-4);
+            yield return null;
+        }
+    }
+    IEnumerator chatranksetting()
+    {
+        chatb.anchoredPosition = new Vector2(282, 0.2f); 
+        ranksettingpanel.anchoredPosition = new Vector2(262, 0);
         while (chatb.anchoredPosition.x > -0.2f)
         {
-            //print(chatb.anchoredPosition.x);
-            //print(chatb.anchoredPosition.y);
-            chatb.anchoredPosition = new Vector2(chatb.anchoredPosition.x - 1, 0.2f);
-
+            chatb.anchoredPosition = new Vector2(chatb.anchoredPosition.x - 4, 0.2f);
             yield return null;
         }
-    }
-    IEnumerator ranksetting()
-    {
-        ranksettingpanel.anchoredPosition = new Vector2(262, 0);
         while (ranksettingpanel.anchoredPosition.x > -0.5f)
         {
-           // print(ranksettingpanel.anchoredPosition.x);
-            //print(ranksettingpanel.anchoredPosition.y);
-            ranksettingpanel.anchoredPosition = new Vector2(ranksettingpanel.anchoredPosition.x - 1.5f,0);
-
+            ranksettingpanel.anchoredPosition = new Vector2(ranksettingpanel.anchoredPosition.x - 4,0);
             yield return null;
         }
     }
-
+    
     IEnumerator Delay(List<GameObject> obj)
     {
         int i = 0;
